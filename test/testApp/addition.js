2c81c9e2bfee132e2330b70c8a3f876157beb115
QualityWorksCG <QualityWorksCG@users.noreply.github.com>
2017-05-08 10:02:14 -0500

    Changed var name 'ele' to 'element'

--- test/testApp/addition.js
+++ test/testApp/addition.js
@@ -1,4 +1,4 @@
-var ele  = require('../../common/elements.js');
+var element = require('../../common/elements.js');
 var client = browser;
 
 describe('Testing addition functionality', function(){
@@ -7,20 +7,20 @@ describe('Testing addition functionality', function(){
     
     //ES6 generator - yield
     it('should fill two fields with numbers', function* () { 
-        yield client.setValue(ele.txt1,values[0])
+        yield client.setValue(element.txt1,values[0])
         yield client.pause(5000)
-        var val = yield client.getValue(ele.txt1)
+        var val = yield client.getValue(element.txt1)
         val.should.not.equal(null);
         
-        yield client.setValue(ele.txt2,values[1])
+        yield client.setValue(element.txt2,values[1])
         yield client.pause(5000)
-        var val2 = yield client.getValue(ele.txt2)
+        var val2 = yield client.getValue(element.txt2)
         val2.should.not.equal(null);
     });
     
     it('should compute the numbers and produce correct addition', function* () {
-        yield client.click(ele.btnSum)
-        var ans = yield client.getText(ele.txtAns)
+        yield client.click(element.btnSum)
+        var ans = yield client.getText(element.txtAns)
         parseInt(ans, 10).should.equal(values[0] + values[1]);
     });
 });

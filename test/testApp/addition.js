4bd7999baf8e0f5e346b101551df9ebb741c6c4e
QualityWorksCG <QualityWorksCG@users.noreply.github.com>
2017-05-19 06:27:38 -0500

    Update addition.js

--- test/testApp/addition.js
+++ test/testApp/addition.js
@@ -1,26 +1,26 @@
-var element = require('../../common/elements.js');
+var elements = require('../../common/elements.js');
 var client = browser;
 
 describe('Testing addition functionality', function(){
     this.timeout(300000);
-    var values =[3,6];
+    var numbers =[3,6];
     
     //ES6 generator - yield
     it('should fill two fields with numbers', function* () { 
-        yield client.setValue(element.txt1,values[0])
+        yield client.setValue(elements.txt1,numbers[0])
         yield client.pause(5000)
-        var val = yield client.getValue(element.txt1)
-        val.should.not.equal(null);
+        var textOneValue = yield client.getValue(elements.txt1)
+        textOneValue.should.not.equal(null);
         
-        yield client.setValue(element.txt2,values[1])
+        yield client.setValue(elements.txt2,numbers[1])
         yield client.pause(5000)
-        var val2 = yield client.getValue(element.txt2)
-        val2.should.not.equal(null);
+        var textTwoValue = yield client.getValue(elements.txt2)
+        textTwoValue.should.not.equal(null);
     });
     
     it('should compute the numbers and produce correct addition', function* () {
-        yield client.click(element.btnSum)
-        var ans = yield client.getText(element.txtAns)
-        parseInt(ans, 10).should.equal(values[0] + values[1]);
+        yield client.click(elements.btnSum)
+        var textAnwserValue = yield client.getText(elements.txtAns)
+        parseInt(textAnwserValue, 10).should.equal(numbers[0] + numbers[1]);
     });
 });
